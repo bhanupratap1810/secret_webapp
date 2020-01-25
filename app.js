a801f5ccbcf42f27d3fa999bfb993365ef37c23f
Bhanu <bhanupratapbd1@gmail.com>
2020-01-26 04:23:40 +0530

    Final commit

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://git.heroku.com/enigmatic-shore-24930.git/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
